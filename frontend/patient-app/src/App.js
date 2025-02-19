8501d29e69483a9d405614da911c4707cbbc4cc4
radicalz1 <142083579+radicalz1@users.noreply.github.com>
2025-02-19 12:54:05 +0700

    Create App.js

---
+++ frontend/patient-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChatWindow from './components/ChatWindow';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Patient Chat</h1>
+      <ChatWindow />
+    </div>
+  );
+}
+
+export default App;
