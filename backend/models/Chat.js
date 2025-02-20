2f45a3ae53a71e38be9bda26eb5d419ac8be56d4
radicalz1 <142083579+radicalz1@users.noreply.github.com>
2025-02-20 11:02:59 +0700

    Create Chat.js

---
+++ backend/models/Chat.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const ChatSchema = new mongoose.Schema({
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message', default: null },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Chat', ChatSchema);
