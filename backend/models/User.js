d906e405f023b086fe81c381084a72d94c84ce58
radicalz1 <142083579+radicalz1@users.noreply.github.com>
2025-02-20 11:02:40 +0700

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email:    { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  role: { type: String, enum: ['patient', 'pharmacist', 'admin'], default: 'patient' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
